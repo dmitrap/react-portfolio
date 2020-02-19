3b5dc95cd9fada865ddf2173f57384ad3de19556
dmitrap <dmitrap@gmail.com>
2020-02-19 03:09:49 +0300

    Updates

---
+++ precache-manifest.858d31b467252ce97325177120888288.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f39dda5ac0b5f909c7ed067d0342664",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d94338fae103d4451d54",
+    "url": "/react-portfolio/static/css/main.dc164594.chunk.css"
+  },
+  {
+    "revision": "aed8ea0bac26318706ad",
+    "url": "/react-portfolio/static/js/2.f151f333.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-portfolio/static/js/2.f151f333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94338fae103d4451d54",
+    "url": "/react-portfolio/static/js/main.d16aee32.chunk.js"
+  },
+  {
+    "revision": "9f289a7f9c113993272b",
+    "url": "/react-portfolio/static/js/runtime-main.f68edf73.js"
+  },
+  {
+    "revision": "00dc0ac6f02bc23933b398c3563284dc",
+    "url": "/react-portfolio/static/media/git.00dc0ac6.svg"
+  },
+  {
+    "revision": "c8982e0a3e6cd0bb6084a7802fed65dd",
+    "url": "/react-portfolio/static/media/html.c8982e0a.svg"
+  },
+  {
+    "revision": "41c6f7f657b4f92a3ac0db2d6f85bcf6",
+    "url": "/react-portfolio/static/media/js.41c6f7f6.svg"
+  },
+  {
+    "revision": "6934d4c4f57d65b6ee3afbfa8d1cec63",
+    "url": "/react-portfolio/static/media/linkedin.6934d4c4.svg"
+  },
+  {
+    "revision": "65b4bff82e6d20f4e43d0e78678b54dc",
+    "url": "/react-portfolio/static/media/profile.65b4bff8.jpg"
+  },
+  {
+    "revision": "cabf0ea927de3fc45c31bfab818bf44e",
+    "url": "/react-portfolio/static/media/react.cabf0ea9.svg"
+  },
+  {
+    "revision": "87d09bfecaa0cda1a6f4ea0e320b8cf2",
+    "url": "/react-portfolio/static/media/redux.87d09bfe.svg"
+  },
+  {
+    "revision": "99d0007553b25d8a6f85b0b7ef32fc4a",
+    "url": "/react-portfolio/static/media/social_network.99d00075.jpeg"
+  },
+  {
+    "revision": "9ca51d2ac295d3538a2173763f5c7e70",
+    "url": "/react-portfolio/static/media/telegram.9ca51d2a.svg"
+  },
+  {
+    "revision": "aa70e5acfce54ec547cac048b7667421",
+    "url": "/react-portfolio/static/media/todo.aa70e5ac.jpeg"
+  }
+]);
